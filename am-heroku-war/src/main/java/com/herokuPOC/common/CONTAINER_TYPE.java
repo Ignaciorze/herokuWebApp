f8b1a8e951d132f1982ed7ba00a05128b8f3d030
cgranadosr <carmen.granados.rico@pwc.com>
2021-09-09 10:13:40 +0200

    added all containers type

--- am-heroku-war/src/main/java/com/herokuPOC/common/CONTAINER_TYPE.java
+++ am-heroku-war/src/main/java/com/herokuPOC/common/CONTAINER_TYPE.java
@@ -2,7 +2,17 @@ package com.herokuPOC.common;
 
 
 public enum CONTAINER_TYPE {
+    EMPTY,
+    ACCOUNT,
+    ACTIVITY,
     CONTACT,
-    ACCOUNT
+    CONTRACT,
+    EXCHANGE_RATES,
+    GDS_IATA,
+    INSTALLED_PRODUCTS,
+    OFFICE_ID,
+    OPPORTUNITY,
+    SOLD_PRODUCT,
+    TECNICAL_INFO
 }
 
